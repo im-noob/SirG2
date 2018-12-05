6c31ea98fca7d7676752583468f277d89209aace
amriteshonly4u <amriteshonly4u@gmail.com>
2018-12-05 11:31:14 -0500

    moving queiotn done now timer and select quesiton

--- screens/ShowTestScreen.js
+++ screens/ShowTestScreen.js
@@ -18,20 +18,71 @@ export default class ShowTestScreen extends Component {
         super(props);
         this.state = {
             renderCoponentFlag: false,
+            quenstion_set:[
+                [
+                    {optionNO:'Q',content:'Grand Central Terminal, Park Avenue, New York is the world\'s',type:'text'},
+                    {optionNO:'1.',content:'largest railway station',type:'text'},
+                    {optionNO:'2.',content:'highest railway station',type:'text'},
+                    {optionNO:'3.',content:'longest railway station',type:'text'},
+                    {optionNO:'4.',content:'None of the above',type:'text'},
+                ],
+                [
+                    {optionNO:'Q',content:'Entomology is the science that studies',type:'text'},
+                    {optionNO:'1.',content:'Behavior of human beings',type:'text'},
+                    {optionNO:'2.',content:'Insects',type:'text'},
+                    {optionNO:'3.',content:'The origin and history of technical and scientific terms',type:'text'},
+                    {optionNO:'4.',content:'The formation of rocks',type:'text'},
+                ],
+                [
+                    {optionNO:'Q',content:'Eritrea, which became the 182nd member of the UN in 1993, is in the continent of',type:'text'},
+                    {optionNO:'1.',content:'Asia',type:'text'},
+                    {optionNO:'2.',content:'Africa',type:'text'},
+                    {optionNO:'3.',content:'Europe',type:'text'},
+                    {optionNO:'4.',content:'Australia',type:'text'},
+                ],
+                [
+                    {optionNO:'Q',content:'Garampani sanctuary is located at',type:'text'},
+                    {optionNO:'1.',content:'Junagarh, Gujarat',type:'text'},
+                    {optionNO:'2.',content:'Diphu, Assam',type:'text'},
+                    {optionNO:'3.',content:'Kohima, Nagaland',type:'text'},
+                    {optionNO:'4.',content:'Gangtok, Sikkim',type:'text'},
+                ],
+                [
+                    {optionNO:'Q',content:'For which of the following disciplines is Nobel Prize awarded?',type:'text'},
+                    {optionNO:'1.',content:'Physics and Chemistry',type:'text'},
+                    {optionNO:'2.',content:'Physiology or Medicine',type:'text'},
+                    {optionNO:'3.',content:'Literature, Peace and Economics',type:'text'},
+                    {optionNO:'4.',content:'All of the above',type:'text'},
+                ],
+            ],
+            question_active_no:0,
         }
     }
     componentDidMount() {
         setTimeout(() => {this.setState({renderCoponentFlag: true})}, 0);
     }
-
+    changeQuestion = (input) =>{
+        let question_active_no = this.state.question_active_no
+        if(input == 'prev'){
+            if(question_active_no == 0){
+                alert("This is First Question");
+                return;
+            }
+            this.setState({
+                question_active_no: question_active_no -1
+            });
+        }else if(input == 'next'){
+            if(question_active_no == this.state.quenstion_set.length-1){
+                alert("This is Last Question");
+                return;
+            }
+            this.setState({
+                question_active_no: question_active_no + 1
+            });
+        }
+    }
     render() {
-        var items = [
-            {optionNO:'Q',content:'Grand Central Terminal, Park Avenue, New York is the world\'s',type:'text'},
-            {optionNO:'1.',content:'largest railway station',type:'text'},
-            {optionNO:'2.',content:'highest railway station',type:'text'},
-            {optionNO:'3.',content:'longest railway station',type:'text'},
-            {optionNO:'4.',content:'None of the above',type:'text'},
-          ];
+        var items = this.state.quenstion_set[this.state.question_active_no];
         const {renderCoponentFlag} = this.state;
         console.log(height);
         console.log(width);
@@ -84,10 +135,10 @@ export default class ShowTestScreen extends Component {
                             </View>
                             <View style={{height:20,}}>
                                 <View style={{flexDirection:'row',}}>
-                                    <TouchableOpacity style={{backgroundColor:'#000',flex:1,alignContent:'center',alignSelf:'center',alignItems:'center'}}>
+                                    <TouchableOpacity style={{backgroundColor:'#000',flex:1,alignContent:'center',alignSelf:'center',alignItems:'center'}} onPress={()=>{this.changeQuestion('prev')}}>
                                         <Text style={{paddingVertical: 5,fontSize:25,color:'white',alignSelf:'center',alignItems:'center',alignContent:'center'}}><Icon name="less-than" style={{fontSize:30}}/> PREV</Text>
                                     </TouchableOpacity>
-                                    <TouchableOpacity style={{backgroundColor:'#0a60ff',flex:1,alignContent:'center',alignSelf:'center',alignItems:'center'}}>
+                                    <TouchableOpacity style={{backgroundColor:'#0a60ff',flex:1,alignContent:'center',alignSelf:'center',alignItems:'center'}} onPress={()=>{this.changeQuestion('next')}}>
                                         <Text style={{paddingVertical: 5,fontSize:25,color:'white',alignSelf:'center',alignItems:'center',alignContent:'center'}}>NEXT<Icon name="greater-than" style={{fontSize:30}}/></Text>
                                     </TouchableOpacity>
                                 </View>
